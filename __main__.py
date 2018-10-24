cdf3f6cb6afd0085dc22504ffc35fdeb67a226fa
Harris B <legostarwarsguy02@gmail.com>
2018-10-23 20:45:36 -0700

    Update MAIN to include more extensive analysis

--- __main__.py
+++ __main__.py
@@ -8,9 +8,16 @@ import os
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
 import selenium
 chromedriver = 'C:\Python27\selenium\webdriver\chromedriver.exe'
-browser = webdriver.Chrome(chromedriver)
+chrome_options = Options()
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+browser = webdriver.Chrome(chromedriver,   chrome_options=chrome_options)
 
 
 def checkLicensePlate(stateInitials, licensePlate):
@@ -19,4 +26,10 @@ def checkLicensePlate(stateInitials, licensePlate):
 	browser.find_element_by_id('state').click()
 	browser.find_element_by_xpath("//*[contains(text(), '%s')]" % stateInitials).click()
 	browser.find_element_by_id('plateSearch').click()
-checkLicensePlate("CA","6JVC694")
+	time.sleep(1.0)
+	element = browser.find_element_by_id('test_vinSummary_carSpecification_$4')
+	summary = element.get_attribute('innerHTML')
+	carName = summary.split('<h2>')[1].split('</h2>')[0].replace('&nbsp;',' ')
+	print("Is your car a %s?" % carName)
+
+checkLicensePlate("CA","ERROR405")
